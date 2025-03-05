e5321e828171a58d2e6b65f255cdef224bf49d81
abhaytelerapp <abhay@telerapps.com>
2025-03-05 18:20:47 +0530

    first commit

--- dist/App.js
+++ dist/App.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _Viewer = _interopRequireDefault(require("./components/viewer/Viewer.jsx"));
+var _Viewer = _interopRequireDefault(require("./components/viewer/Viewer"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function App() {
   return /*#__PURE__*/React.createElement(_Viewer.default, null);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   module: {
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/components/image/icon/lite'),
+                    to: path.resolve(__dirname, 'dist/components/image/icon/lite')
+                }
+            ]
+        })
+    ],
     rules: [
       {
         test: /\.(js|jsx)$/,
