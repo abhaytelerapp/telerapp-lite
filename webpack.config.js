c3b75f348acffbf1dfbf795d718b4dc1804e830f
abhaytelerapp <abhay@telerapps.com>
2025-03-05 18:35:33 +0530

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  entry: "./src/index.js", // Ensure entry point is correct
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // Ensure Webpack generates output
+  },
   module: {
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/components/image/icon/lite'),
-                    to: path.resolve(__dirname, 'dist/components/image/icon/lite')
-                }
-            ]
-        })
-    ],
     rules: [
       {
         test: /\.(js|jsx)$/,
@@ -20,10 +15,37 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              outputPath: "images",
+              publicPath: "images",
+            },
+          },
+        ],
+      },      
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/components/image/icon/lite"),
+          to: "images", // This will copy to "dist/images/"
+          globOptions: {
+            ignore: ["**/.DS_Store"],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
 };
