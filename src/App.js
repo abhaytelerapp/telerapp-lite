7a26c20b09f8203ee994c248aa1dcdc0a6f93325
abhaytelerapp <abhay@telerapps.com>
2025-03-05 16:29:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Viewer from './components/viewer/Viewer.jsx';
+
+function App() {
+  return (
+    <>
+      <Viewer/>
+    </>
+  );
+}
+
+export default App;
