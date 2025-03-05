e583a5f57cab570caf71b9451dd1bf102dd51ed0
abhaytelerapp <abhay@telerapps.com>
2025-03-05 17:52:58 +0530

    first commit

--- dist/App.js
+++ dist/App.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _logo = _interopRequireDefault(require("./logo.svg"));
-require("./App.css");
 var _Viewer = _interopRequireDefault(require("./components/viewer/Viewer.jsx"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function App() {
--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Viewer from "./components/viewer/Viewer.jsx";
 
 function App() {
