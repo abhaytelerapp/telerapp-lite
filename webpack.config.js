06f2aad950ea591ef0bd3f6ccaf1bcdfeee5d41b
abhaytelerapp <abhay@telerapps.com>
2025-03-05 17:20:57 +0530

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,13 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: [
-          path.resolve(__dirname, "src"),
-          path.resolve(__dirname, "node_modules/@abhaytelerapp/telerapp-lite"), // Ensure your plugin is transpiled
-        ],
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
+        exclude: /node_modules\/(?!@abhaytelerapp\/telerapp-lite)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
       },
     ],
   },
