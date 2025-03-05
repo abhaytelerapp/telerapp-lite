f5c9048e8285d5da5f28dc689c374d4cd53bc1b3
abhaytelerapp <abhay@telerapps.com>
2025-03-05 16:39:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 // import Viewer from './components/viewer/Viewer.jsx';
 
+export { App };
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
