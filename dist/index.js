822f71ffb3e131e4aeeea3820bc9035c43c66ce6
abhaytelerapp <abhay@telerapps.com>
2025-04-26 17:18:58 +0530

    completed add props

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,5 @@ Object.defineProperty(exports, "App", {
     return _App.default;
   }
 });
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
 var _App = _interopRequireDefault(require("./App"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-const root = _client.default.createRoot(document.getElementById('root'));
-root.render(/*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_App.default, null)));
